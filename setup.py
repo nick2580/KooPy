234984a910e0eae1fac1ad7b17c019e89972be3c
Nikhil Bhamere <nikhil.bhamere@wherescape.com>
2021-12-23 02:38:29 +0530

    Usage and Installation Instructions

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'kooPy',         # How you named your package folder (MyLib)
   packages = ['kooPy'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'KooPy: Koo for Python!',   # Give a short description about your library
   author = 'Nikhil',                   # Type in your name
   author_email = 'nikhil.bhamere@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nick2580',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nick2580/KooPy/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nick2580/KooPy/archive/refs/tags/0.2.tar.gz',    # I explain this later on
   keywords = ['kooapp', 'koo', 'koopy'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
